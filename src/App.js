3a8ad9e24f89ced4c4ac3d9791956c3f1ca31939
roshad <roshad87a9@gmail.com>
2019-06-09 20:03:59 +0800

    cant resolve

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-
-
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+
 
 import Navbar from './component/navbar'
 
@@ -15,5 +15,5 @@ function App() {
 		</Router>
 	);
 }
-
+ 
 export default App;
