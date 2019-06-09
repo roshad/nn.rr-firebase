5f4bcb0cf00cfc91a8e825689715c8de030d4baa
roshad <roshad87a9@gmail.com>
2019-06-09 16:09:00 +0800

    done

---
+++ src/configFb.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const config = {
+    apiKey: "AIzaSyCIPG_xmq2Tm-vwu5y2YjcBUbC-rXHxgxI",
+    authDomain: "nn-cms-c7400.firebaseapp.com",
+    databaseURL: "https://nn-cms-c7400.firebaseio.com",
+    projectId: "nn-cms-c7400",
+    storageBucket: "nn-cms-c7400.appspot.com",
+    messagingSenderId: "928895076809",
+    appId: "1:928895076809:web:8e6f61d8a9911bc1"
+};
+
+firebase.initializeApp(config)
+
+
+export default firebase
