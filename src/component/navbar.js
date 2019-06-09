e39301b2c9210eeda8dae1129a21abd33a513d69
roshad <roshad87a9@gmail.com>
2019-06-09 10:44:52 +0800

    15

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -4,28 +4,36 @@ import { Link } from 'react-router-dom'
 
 function Navbar(props) {
     return (
-        // <nav>
-        //     <div className='navbar-wrapper'>
-        //         <Link className="brand-logo">PMS</Link>
-        //         <a href="#" class="brand-logo">Logo</a>
-        //         <ul id="nav-mobile" class="right hide-on-med-and-down">
-        //             <li><a href="sass.html">Sass</a></li>
-        //             <li><a href="badges.html">Components</a></li>
-        //             <li><a href="collapsible.html">JavaScript</a></li>
-        //         </ul>
-        //     </div>
-        // </nav>
         <nav>
-        <div class="nav-wrapper">
-          <a href="#" className="brand-logo left">Logo</a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
-          </ul>
-        </div>
-      </nav>
+            <div class='navbar-wrapper'>
+                <div class="container">
+                    <Link to='' class="brand-logo">PMS</Link>
+
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Sass</a></li>
+                        <li><a href="badges.html">Components</a></li>
+                        <li><a href="collapsible.html">JavaScript</a></li>
+                    </ul>
+                    <In/>
+                </div>
+
+            </div>
+        </nav>
+
     )
 }
 
+function In(props) {
+    return (
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><Link >New Project</Link></li>
+            <li><Link >Log Out</Link></li>
+            <li><Link className='btn-floating'>Avatar</Link></li>
+        </ul>
+    )
+}
+
+function Out(props) {
+
+}
 export default Navbar;
