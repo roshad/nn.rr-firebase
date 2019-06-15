32a7dc500eae8467630d5ca375ed67cdd7e430f9
roshad <roshad87a9@gmail.com>
2019-06-15 22:47:07 +0800

    done

--- src/actionProject.js
+++
@@ -1,6 +0,0 @@
-const createProject = (project) => (dispatch, { getFirestore, getFirebase }) => {
-    const fireStore = getFirestore()
-    fireStore.collection('project').add({
-        mother:'die'
-    })
-}
---
+++ src/creatorProject.js
@@ -0,0 +1,8 @@
+export const creatorProject = (form) => (dispatch,getState, { getFirestore, getFirebase }) => {
+    
+    
+    const fireStore = getFirestore()
+    fireStore.collection('project').add(form)
+
+}
+
